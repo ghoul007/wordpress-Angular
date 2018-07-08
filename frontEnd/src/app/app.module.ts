b9800500d62cd53325bb26d5d4fb99e583cb3fb4
ahmed <ahmedgh@rosafian.com>
2018-07-08 23:26:14 +0100

    feat (src): add post componenet

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -1,31 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'
 import {
   WpApiModule,
   WpApiLoader,
   WpApiStaticLoader
 } from 'wp-api-angular'
-import { HttpClient } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PostsComponent } from './pages/posts/posts.component';
+import { PostComponent } from './pages/post/post.component';
+import { WpService } from './services/wp.service';
 
-export function WpApiLoaderFactory(http ) {
+export function WpApiLoaderFactory(http) {
   return new WpApiStaticLoader(http, 'http://localhost/wp-json/', /* namespace is optional, default: '/wp/v2' */);
 }
+
+const appRoutes: Routes = [
+  {
+    path: '', component: PostsComponent
+  },
+  {
+    path: ':id', component: PostComponent
+  }
+]
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(appRoutes),
     WpApiModule.forRoot({
       provide: WpApiLoader,
       useFactory: (WpApiLoaderFactory),
-      deps: [HttpClient]
+      deps: [HttpClientModule]
     })
   ],
-  providers: [],
+  providers: [WpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
